4c678f3b5043fb9bf305ba7d8fc46d7e0b940ff4
azizurrahman-zero <azizurrahman.zero@gmail.com>
2022-05-05 14:45:45 +0600

    book data loaded

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.use(bodyParser.json());
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_UPass}@cluster0.rhdf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const bookCollection = client.db('bBook').collection('book');
+
+        app.get('/books', async (req, res) => {
+            const query = {};
+            const cursor = bookCollection.find(query);
+            const books = await cursor.toArray();
+            res.send(books);
+        })
+    }
+    finally {
+
+    }
+}
+
 run().catch(console.dir);
 
 
